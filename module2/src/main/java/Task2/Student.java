e764e0d241bd9f83331d860e2af4568f14c5501b
ronalful <ronalful@yandex.ru>
2025-10-25 08:45:11 +0300

    feat(TASK-1253): feat overloading constructors

---
+++ module2/src/main/java/Task2/Student.java
@@ -0,0 +1,55 @@
+package Task2;
+
+public class Student {
+    private String name;
+    private int age;
+    private double averageGrade;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student(String name, int age, double averageGrade) {
+        this.name = name;
+        this.age = age;
+        this.averageGrade = averageGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        if (averageGrade < 0 || averageGrade > 10) {
+            throw new IllegalArgumentException("averageGrade должно быть от 0 до 10");
+        }
+        this.averageGrade = averageGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", age=" + age +
+                ", averageGrade=" + averageGrade;
+    }
+}
