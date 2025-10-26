582d8da6f94f2639dbcf70cf683d365dd4792457
ronalful <ronalful@yandex.ru>
2025-10-26 11:04:46 +0300

    feat(TASK-1259): added Request parameters (@RequestParam)

--- module5/src/main/java/com/example/module5/task2To3/BookController.java
+++ module5/src/main/java/com/example/module5/task2To3/BookController.java
@@ -2,6 +2,7 @@ package com.example.module5.task2To3;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,13 @@ public class BookController {
 
     @GetMapping("/books/{id}")
     public Book getBookById(@PathVariable int id) {
+        System.out.println("Id книги: " + id);
         return new Book("Crime and Punishment", 1866);
     }
 
-
+    @GetMapping("/books/search")
+    public Book getBookById(@RequestParam(required = false) String title) {
+        System.out.println("Параметр title: " + title);
+        return new Book("War and Peace", 1867);
+    }
 }
