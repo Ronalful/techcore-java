da18b0ab002a9fe41b9a24dd2bd1f3f071826dae
ronal <ronalful@yandex.ru>
2025-10-26 08:25:18 +0300

    feat(1257): added Reading properties from application.properties

--- module4/src/main/java/org/example/module4/task2_3/MessageController.java
+++ module4/src/main/java/org/example/module4/task2_3/MessageController.java
@@ -1,4 +1,4 @@
-package org.example.module4;
+package org.example.module4.task2_3;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
