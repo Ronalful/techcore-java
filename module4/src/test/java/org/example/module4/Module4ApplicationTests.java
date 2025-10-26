f64e37f967d92a2208de24752d2f7daf258b1883
ronal <ronalful@yandex.ru>
2025-10-26 08:11:02 +0300

    feat(1257): Creating a project using Spring Initializer

---
+++ module4/src/test/java/org/example/module4/Module4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.module4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
