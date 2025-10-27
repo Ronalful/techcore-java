ca9641088ce8fdb3aceb42b4c52f092e994fc324
ronalful <ronalful@yandex.ru>
2025-10-27 13:13:28 +0300

    feat(TASK-1263): added Spring Caching Abstraction (@EnableCaching)

--- module6Advanced/src/main/java/com/example/module6advanced/Module6AdvancedApplication.java
+++ module6Advanced/src/main/java/com/example/module6advanced/Module6AdvancedApplication.java
@@ -2,8 +2,10 @@ package com.example.module6advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Module6AdvancedApplication {
 
     public static void main(String[] args) {
