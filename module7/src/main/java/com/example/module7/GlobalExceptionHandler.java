ba7ad8fff068376ea8a3c33f3e529a0eb18825ca
ronalful <ronalful@yandex.ru>
2025-10-28 09:02:27 +0300

    feat(TASK-1265): added Centralized processing of validation errors

--- module7/src/main/java/com/example/module7/GlobalExceptionHandler.java
+++ module7/src/main/java/com/example/module7/GlobalExceptionHandler.java
@@ -4,8 +4,13 @@ import com.example.module7.author.AuthorNotFoundException;
 import com.example.module7.book.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +22,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<?> handle(AuthorNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    //Task 2
+    @ExceptionHandler
+    public ResponseEntity<?> handle(MethodArgumentNotValidException e) {
+        List<Map<String, String>> errors = e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> Map.of(
+                        "field", fieldError.getField(),
+                        "message", fieldError.getDefaultMessage()
+                ))
+                .toList();
+        Map<String, Object> body = new HashMap<>();
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
