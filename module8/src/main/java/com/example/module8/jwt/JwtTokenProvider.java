e267977e909d73306d2eacbf6c5adb07fd71e310
ronalful <ronalful@yandex.ru>
2025-10-28 18:05:00 +0300

    feat(TASK-1267): added Utilities for generating and validating JWT

---
+++ module8/src/main/java/com/example/module8/jwt/JwtTokenProvider.java
@@ -0,0 +1,80 @@
+package com.example.module8.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    @Value("${app.jwtSecret}")
+    private String secretKey;
+    @Value("${app.jwtExpiration}")
+    private long jwtExpiration;
+
+    private SecretKey signInKey;
+
+    @PostConstruct
+    public void initKey() {
+        signInKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration
+    ) {
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(signInKey)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(signInKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
