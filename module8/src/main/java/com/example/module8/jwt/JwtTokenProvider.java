3a4fef1d51c6d8b8374d83a03a2ae61a8bc22dae
ronalful <ronalful@yandex.ru>
2025-10-28 18:21:21 +0300

    feat(TASK-1267): added Creating a JWT Filter

--- module8/src/main/java/com/example/module8/jwt/JwtTokenProvider.java
+++ module8/src/main/java/com/example/module8/jwt/JwtTokenProvider.java
@@ -57,6 +57,10 @@ public class JwtTokenProvider {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
