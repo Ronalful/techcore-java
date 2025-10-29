1ffce69eef058a7daf4aef11e75bd325b287c0cf
ronalful <ronalful@yandex.ru>
2025-10-29 17:34:32 +0300

    feat(TASK-1269): added Repository Integration Testing (@DataJpaTest)

---
+++ module9/src/test/java/com/example/module9/book/BookRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.module9.book;
+
+import com.example.module9.author.Author;
+import com.example.module9.author.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Test
+    void testSaveAndFindBookById() {
+        Author author = Author.builder()
+                .name("John Doe")
+                .build();
+        author = authorRepository.save(author);
+
+        Book book = Book.builder()
+                .title("test")
+                .publicationYear(2025)
+                .author(author)
+                .build();
+        book = bookRepository.save(book);
+
+        Optional<Book> foundBook = bookRepository.findById(book.getId());
+
+        Assertions.assertThat(foundBook).isPresent();
+        Assertions.assertThat(foundBook.get().getTitle()).isEqualTo("test");
+        Assertions.assertThat(foundBook.get().getPublicationYear()).isEqualTo(2025);
+        Assertions.assertThat(foundBook.get().getAuthor().getName()).isEqualTo("John Doe");
+    }
+}
