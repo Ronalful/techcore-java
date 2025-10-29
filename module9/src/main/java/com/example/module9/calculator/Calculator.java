b04dcc6be413859e93fc61e48fd4a37a10d5f480
ronalful <ronalful@yandex.ru>
2025-10-29 17:08:25 +0300

    feat(TASK-1269): added Testing for Exceptions

--- module9/src/main/java/com/example/module9/calculator/Calculator.java
+++ module9/src/main/java/com/example/module9/calculator/Calculator.java
@@ -8,4 +8,9 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Divide by zero");
+        return a / b;
+    }
 }
--- module9/src/test/java/com/example/module9/calculator/CalculatorTest.java
+++ module9/src/test/java/com/example/module9/calculator/CalculatorTest.java
@@ -25,4 +25,10 @@ class CalculatorTest {
         Assertions.assertEquals(-1, calculator.subtract(1, 2));
         Assertions.assertEquals(1, calculator.subtract(-1, -2));
     }
+
+    @Test
+    void testDivide() {
+        Assertions.assertEquals(3, calculator.divide(6, 2));
+        Assertions.assertThrows(ArithmeticException.class, () -> calculator.divide(12, 0));
+    }
 }
